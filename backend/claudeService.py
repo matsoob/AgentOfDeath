d015ff47b7a5e9023a7de30278a7482e227cd2f4
matsoob <emma.matsubara@autogenai.com>
2023-11-04 19:34:54 +0000

    add cache for claude calls

--- backend/claudeService.py
+++ backend/claudeService.py
@@ -1,3 +1,4 @@
+from functools import cache
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
 
@@ -7,20 +8,20 @@ class ClaudeService:
 
     def getPersonalMessage(self, name_of_deceased: str) -> str:
         print("Beginning of getPersonalMessage")
-        prompt = (
-            f"\n\nHuman: Please generate a gentle welcome message for our website, where we help this person deal with the death of <name>{name_of_deceased}</name> \n\nAssistant: ",
-        )
+        prompt = f"\n\nHuman: Please generate a gentle welcome message for our website, where we help this person deal with the death of <name>{name_of_deceased}</name> \n\nAssistant: "
+        print(prompt)
         response = self._make_claude_call(prompt)
         print("End of getPersonalMessage")
         return response
 
     def custom_prompt(self, custom_prompt: str) -> str:
         print("Beginning of custom prompt")
-        prompt = (f"\n\nHuman: {custom_prompt} \n\nAssistant: ",)
+        prompt = f"\n\nHuman: {custom_prompt} \n\nAssistant: "
         response = self._make_claude_call(prompt)
         print("End of custom prompt")
         return response
 
+    @cache
     def _make_claude_call(self, prompt: str) -> str:
         completion = self.client.completions.create(
             model="claude-2",
