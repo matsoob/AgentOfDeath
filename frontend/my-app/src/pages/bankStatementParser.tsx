d8ed9d4811b0fc9fe8d1d505e044aa6c878e952f
matsoob <emma.matsubara@autogenai.com>
2023-11-05 10:43:53 +0000

    table add working

--- frontend/my-app/src/pages/bankStatementParser.tsx
+++ frontend/my-app/src/pages/bankStatementParser.tsx
@@ -4,6 +4,7 @@ import { PdfParser } from "./pdf";
 interface BankStatementProps {}
 
 async function addSubscription(sub_name: string) {
+  console.log(sub_name);
   try {
     // name_of_sub: str, status
     const result = await fetch(
@@ -19,33 +20,35 @@ async function addSubscription(sub_name: string) {
   }
 }
 
-function listOfSubscriptionsToAdd(subscriptions: Array<string>) {
-  return (
-    <div className="mb-6">
-      <table className="min-w-full border-collapse border border-gray-300">
-        <thead className="bg-gray-200">
-          <tr>
-            <th className="py-2 px-4 text-left">Name of Subscription</th>
-            <th className="py-2 px-4 text-center">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {subscriptions.map((val, key) => (
-            <tr key={key} className="hover:bold">
-              <td className="py-2 px-4">{val}</td>
-              <td className="py-2 px-4 text-center">Do you want this added?</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
 export function BankStatementParser(props: BankStatementProps) {
   const [submittedResponse, setSubmittedResponse] = useState(
     [] as Array<string>
   );
+
+  const listOfSubscriptionsToAdd = (subscriptions: Array<string>) => {
+    return (
+      <div className="mb-6">
+        <table className="min-w-full border-collapse border border-gray-300">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="py-2 px-4 text-left">Name of Subscription</th>
+              <th className="py-2 px-4 text-center">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subscriptions.map((val, key) => (
+              <tr key={key} className="hover:bold">
+                <td className="py-2 px-4">{val}</td>
+                <td className="py-2 px-4 text-center">
+                  <button onClick={() => addSubscription(val)}>Add</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
   return (
     <div>
       <PdfParser
